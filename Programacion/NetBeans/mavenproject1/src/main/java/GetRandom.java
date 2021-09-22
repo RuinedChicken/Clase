1e8c0acc4d8ce4811568be005e4b0b10696beaa8
RuinedChicken <solerfernandezbs@gmail.com>
2021-09-22 20:04:02 +0200

    logos linux, teoria

---
+++ Programacion/NetBeans/mavenproject1/src/main/java/GetRandom.java
@@ -0,0 +1,11 @@
+public class GetRandom {
+
+    public static void main(String[] args) {
+        
+        int dato1 = 6;
+        int dato2 = dato1++;
+        System.out.println(dato2);
+        --dato1;
+        System.out.println(dato2);
+        
+}
