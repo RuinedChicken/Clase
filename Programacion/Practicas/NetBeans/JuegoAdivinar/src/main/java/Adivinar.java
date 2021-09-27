a1707203725ebb75a9341d9410115189bffcf38b
RuinedChicken <solerfernandezbs@gmail.com>
2021-09-27 18:14:52 +0200

    Practica loop

--- Programacion/Practicas/NetBeans/JuegoAdivinar/src/main/java/Adivinar.java
+++ Programacion/Practicas/NetBeans/JuegoAdivinar/src/main/java/Adivinar.java
@@ -3,57 +3,53 @@
  * @author Luis Manuel Soler Fernández
  */
 
+/* El siguiente es un juego de aciertos en el que un usuario
+introduce un número y otro tiene varios intentos para adivinarlo.
+
+Ante cada intento fallido el programa nos dirá si el número a
+adivinar es más alto o más bajo. Contaremos cuántos intentos llevamos.
+
+Cuanto más alto sea el número a adivinar más puntos podemos sacar.  */
+
 import java.util.Scanner;
 
 public class Adivinar {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        /* Hacer un juego de aciertos en el que un usuario
-        introduzca un número y otro tenga varios intentos para adivinarlo.
-        
-        Ante cada intento fallido el programa nos dirá si era más alto o más bajo.
-        Contaremos cuántos intentos llevamos. */
-        
-        /*Caracteristicas adicionales:
-        Puntuacion?
-        Tiempo?
-        Maximo de intentos?
-        Maximo de intentos = puntuacion maxima
-        puntuacion final = puntuacion maxima - intentos
-        facil/dificil
-        */
         
         Scanner lector = new Scanner(System.in);
         
-        System.out.println("Por favor, introduce el número entero que quieres que se adivine");
+        System.out.println("Por favor, introduce el número entero que quieres que se adivine:");
+        
+        // El número a adivinar.
         int numeroSecreto = lector.nextInt();
+        // El número con el que intentamos adivinar.
         int numeroIntento;
         int i = 1;
         int salir = 0;
-        int maximoIntentos;
-        int puntuacionFinal;
+        int puntuacionMaxima = numeroSecreto - (i * 2);
+        int puntuacionObtenida;
         
-        // System.out.println("Por favor, selecciona una dificultad:\n");
-        System.out.println("1. Fácil\n2. Difícil");
         do{
             System.out.println("Introduce un número para intentar adivinar el número secreto: ");
             numeroIntento = lector.nextInt();
             if (numeroIntento == numeroSecreto){
-                System.out.println("¡Felicidades! Adivinaste el número " + numeroSecreto + ". Has necesitado " + i +" intentos.");
+                System.out.println("¡Felicidades! Adivinaste el número " + numeroSecreto + " en tu intento número: " + i + "\n");
                 ++salir;
             }else if (numeroIntento < numeroSecreto){
-                System.out.println("¡Incorrecto! El número secreto es mayor. Este es tu intento número: " + i + ".");
+                System.out.println("¡Incorrecto! El número secreto es mayor. Este es tu intento número: " + i + "\n");
                 ++i;
             }else if (numeroIntento > numeroSecreto){
-                System.out.println("¡Incorrecto! El número secreto es menor. Este es tu intento número: " + i + ".");
+                System.out.println("¡Incorrecto! El número secreto es menor. Este es tu intento número: " + i + "\n");
                 ++i;
             }
         }while (salir != 1);
         
-        
+        lector.close();
+        puntuacionObtenida = numeroSecreto - (i * 2);
+               
+        System.out.println("Tu puntuación final es: " + puntuacionObtenida + "\n");
+        System.out.println("La puntuación máxima es: " + puntuacionMaxima);
     }
     
 }
---
+++ Programacion/Pruebas/Random.java
@@ -0,0 +1,9 @@
+import java.lang.Math;
+
+class Random{
+    public static void main(String args[]){
+        double rand = (Math.random() * 6);
+
+        System.out.println((int)rand);
+    }
+}
